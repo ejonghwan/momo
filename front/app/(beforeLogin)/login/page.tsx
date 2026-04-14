58443714f00401e3661ca34381d83a5b67b1540a
ejonghwan <sun87-1@daum.net>
2026-04-14 18:53:21 +0900

    asd

--- front/app/(beforeLogin)/login/page.tsx
+++ front/app/(beforeLogin)/login/page.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+"use client"
+
+import { useUserStore } from '@/store/front/useUserStore';
+import React, { useEffect } from 'react'
 
 const LoginPage = () => {
+
+  const user = useUserStore((state) => state.user);
+
+  useEffect(() => {
+    console.log('??? user', user)
+  }, [])
+
   return (
     <div>
         로그인 페이지
