88b4340a7335009866a27f87212b09ec5b4a1208
ejonghwan <sun87-1@daum.net>
2026-04-25 11:55:54 +0900

    scss 구조 설정

--- front/src/app/(beforeLogin)/home/page.tsx
+++ front/src/app/(beforeLogin)/home/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import { useUserStore } from '@/store/front/useUserStore'
 import LoginButton from "@/components/LoginButton";
 import LogoutButton from "@/components/LogoutButton";
@@ -10,43 +11,61 @@ import Skeleton from "@/components/style-ui/common/Skeleton";
 const HomePage = () => {
 
   const user = useUserStore((state) => state.user);
-  const isLoggedIn = !!user; // 유저 정보가 있으면 true
+  const isInitialized = useUserStore((state) => state.isInitialized);
+  // const isLoggedIn = !!user; // 유저 정보가 있으면 true
 
-  const [loading, setLoading] = useState(false)
 
-  console.log('user????', user)
-  if (!isLoggedIn) {
-    return <div>
+  // const useIsMounted = () => {
+  //   const [mounted, setMounted] = useState(false);
+  //   useEffect(() => {
+  //     setTimeout(() => { setMounted(true) }, 1000)
+  //   }, []);
+  //   return mounted;
+  // };
 
-      {!user && (
-        <>
-          <LoginButton />
-          <LoginButtonKaKao />
-        </>
-      )}
-      로그인이 필요합니다.
-    </div>;
+  // const is = useIsMounted()
+
+
+
+
+
+  if (!isInitialized) {
+    return (
+      <div className="loading-container">
+        {/* <Skeleton width="50px" height="50px" borderRadius="50%" />
+        <p>사용자 정보를 확인하고 있습니다...</p> */}
+      </div>
+    );
   }
 
-  return <div>
 
-    {loading ? (
+  if (isInitialized && !user) {
+    return (
+      <div>
+        <LoginButton />
+        <LoginButtonKaKao />
+        로그인이 필요합니다.
+      </div>
+    )
+  }
+
+
+  if (isInitialized && user) {
+    return (
       <>
-
         {user.user_metadata?.avatar_url && (
           <>
+            {/* <Skeleton width={"50px"} height={"50px"} borderRadius={"50%"} /> */}
             <div>{user && <LogoutButton />}</div>
             <img src={user.user_metadata.avatar_url} alt="profile" width={50} />
             <div>{user.user_metadata.email}</div>
           </>
-        )}</>
-    ) : (
-      <Skeleton width={"50px"} height={"50px"} borderRadius={"50%"} />
-    )}
-    before Layoutaa
+        )}
+      </>
+    )
+  }
 
 
-  </div>
 };
 
 export default HomePage;
--- front/src/store/front/useUserStore.ts
+++ front/src/store/front/useUserStore.ts
@@ -2,11 +2,13 @@ import { create } from 'zustand'
 import { User } from '@supabase/supabase-js'
 
 interface UserState {
-   user: User | null
+   user: User | null;
+   isInitialized: boolean;
    setUser: (user: User | null) => void
 }
 
 export const useUserStore = create<UserState>((set) => ({
    user: null,
-   setUser: (user) => set({ user }),
+   isInitialized: false,
+   setUser: (user) => set({ user, isInitialized: true }),
 }))
