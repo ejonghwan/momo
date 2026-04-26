221de2476408d6a66dccc5a9fe7e7b3b535c1157
ejonghwan <sun87-1@daum.net>
2026-04-27 00:28:48 +0900

    as

--- front/src/app/(beforeLogin)/home/page.tsx
+++ front/src/app/(beforeLogin)/home/page.tsx
@@ -1,17 +1,10 @@
-"use client"
 
 
-import React, { useEffect, useState } from "react";
-import { useUserStore } from '@/store/front/useUserStore'
-import LoginButton from "@/components/LoginButton";
-import LogoutButton from "@/components/LogoutButton";
-import LoginButtonKaKao from "@/components/LoginButtonKaKao";
-import Skeleton from "@/components/style-ui/common/Skeleton";
+import UserInfo from '@/components/style-ui/user/UserInfo';
 
 const HomePage = () => {
 
-  const user = useUserStore((state) => state.user);
-  const isInitialized = useUserStore((state) => state.isInitialized);
+
   // const isLoggedIn = !!user; // 유저 정보가 있으면 true
 
 
@@ -26,48 +19,9 @@ const HomePage = () => {
   // const is = useIsMounted()
 
 
-  useEffect(() => {
-    console.log(user)
-  }, [])
 
+  return <UserInfo />
 
-  if (!isInitialized) {
-    return (
-      <div className="loading-container">
-        {/* <Skeleton width="50px" height="50px" borderRadius="50%" />
-        <p>사용자 정보를 확인하고 있습니다...</p> */}
-      </div>
-    );
-  }
-
-
-  if (isInitialized && !user) {
-    return (
-      <div>
-        <LoginButton />
-        <LoginButtonKaKao />
-        로그인이 필요합니다.
-      </div>
-    )
-  }
-
-
-  if (isInitialized && user) {
-    return (
-      <>
-        {user.user_metadata?.avatar_url && (
-          <>
-            {/* <Skeleton width={"50px"} height={"50px"} borderRadius={"50%"} /> */}
-            <div>{user && <LogoutButton />}</div>
-            <img src={user.user_metadata.avatar_url} alt="profile" width={50} />
-            <div>{user.user_metadata.email}</div>
-            <div>{user.user_metadata.full_name}</div>
-            <div>{user.app_metadata.provider}</div>
-          </>
-        )}
-      </>
-    )
-  }
 
 
 };
--- front/src/components/style-ui/user/UserInfo.tsx
+++ front/src/components/style-ui/user/UserInfo.tsx
@@ -1,9 +1,54 @@
+"use client"
+
 import React from 'react'
+import LoginButton from '@/components/style-ui/user/LoginButton'
+import LoginButtonKaKao from '@/components/style-ui/user/LoginButtonKaKao'
+import LogoutButton from '@/components/style-ui/user/LogoutButton'
+import { useUserStore } from '@/store/front/useUserStore'
+
 
 const UserInfo = () => {
-   return (
-      <div>UserInfo</div>
-   )
+
+   const user = useUserStore((state) => state.user);
+   const isInitialized = useUserStore((state) => state.isInitialized);
+
+   if (!isInitialized) {
+      return (
+         <div className="loading-container">
+            {/* <Skeleton width="50px" height="50px" borderRadius="50%" />
+           <p>사용자 정보를 확인하고 있습니다...</p> */}
+         </div>
+      );
+   }
+
+
+   if (isInitialized && !user) {
+      return (
+         <div>
+            <LoginButton />
+            <LoginButtonKaKao />
+            로그인이 필요합니다.
+         </div>
+      )
+   }
+
+
+   if (isInitialized && user) {
+      return (
+         <>
+            {user.user_metadata?.avatar_url && (
+               <>
+                  {/* <Skeleton width={"50px"} height={"50px"} borderRadius={"50%"} /> */}
+                  <div>{user && <LogoutButton />}</div>
+                  <img src={user.user_metadata.avatar_url} alt="profile" width={50} />
+                  <div>{user.user_metadata.email}</div>
+                  <div>{user.user_metadata.full_name}</div>
+                  <div>{user.app_metadata.provider}</div>
+               </>
+            )}
+         </>
+      )
+   }
 }
 
 export default UserInfo
